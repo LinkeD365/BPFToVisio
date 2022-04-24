237469bb1f1e16f321cbcfc17719a182d7691147
LinkeD365 <43988771+LinkeD365@users.noreply.github.com>
2022-04-24 14:13:25 +0100

    removing template

--- BPF To Visio/BPFToVIsioCtl.cs
+++ BPF To Visio/BPFToVIsioCtl.cs
@@ -25,39 +25,7 @@ namespace LinkeD365.BPFToVisio
             CloseTool();
         }
 
-        private void tsbSample_Click(object sender, EventArgs e)
-        {
-            // The ExecuteMethod method handles connecting to an
-            // organization if XrmToolBox is not yet connected
-            ExecuteMethod(GetAccounts);
-        }
-
-        private void GetAccounts()
-        {
-            WorkAsync(new WorkAsyncInfo
-            {
-                Message = "Getting accounts",
-                Work = (worker, args) =>
-                {
-                    args.Result = Service.RetrieveMultiple(new QueryExpression("account")
-                    {
-                        TopCount = 50
-                    });
-                },
-                PostWorkCallBack = (args) =>
-                {
-                    if (args.Error != null)
-                    {
-                        MessageBox.Show(args.Error.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    }
-                    var result = args.Result as EntityCollection;
-                    if (result != null)
-                    {
-                        MessageBox.Show($"Found {result.Entities.Count} accounts");
-                    }
-                }
-            });
-        }
+       
 
         /// <summary>
         /// This event occurs when the plugin is closed
